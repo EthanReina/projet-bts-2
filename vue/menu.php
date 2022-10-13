fcbac781157898c921465ebf5eabda0651fc32df
majidbenmerah <99648395+majidbenmerah@users.noreply.github.com>
2022-10-13 14:49:47 +0200

    Update menu.php

--- vue/menu.php
+++ vue/menu.php
@@ -1,2 +1,8 @@
-<!-- Header -->
-testdza
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a href="index.php" class="navbar-brand">Gestionnaire</a>
+            <div class="navbar-nav ms-auto">
+                <a href="#" class="nav-item nav-link"><button class="btn btn-outline-primary">Connexion</button></a>
+            </div>
+    </div>
+</nav>

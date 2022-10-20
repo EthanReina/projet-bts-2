7bfd351e9723c14575553e771b6e4e0da3c476d3
kwtr-bo <115696131+kwtr-bo@users.noreply.github.com>
2022-10-20 15:22:51 +0200

    t

--- controleur/ctlUtilisateur.php
+++ controleur/ctlUtilisateur.php
@@ -26,7 +26,7 @@ switch($action) {
         $email = $_POST['email'];
         $password = $_POST['password'];
         $result = DbUtilisateur::getUser($email, $password);
-        if ($result['validation'] == 1) {
+        if (COUNT($result)>0) {
             $_SESSION['connect'] = 1;
             $_SESSION['email'] = $email;
             header('location: index.php');
@@ -34,6 +34,39 @@ switch($action) {
             echo "<h3 class='text-center'>Les informations de connexion sont incorrectes</h3>";
 
         }
+<<<<<<< Updated upstream
+=======
+
+        break;
+
+
+    // si l'action est profil, on affiche le profil de l'utilisateur (vue v_profilUtilisateur.php)
+    // On récupère les infos de l'utilisateur via la fonction getUserInfo() de la class dbUtilisateur
+    // On récupère les infos du véhicule via la fonction getInfoVehicule() de la class dbUtilisateur
+    case 'profil':
+            
+            if (isset($_SESSION['connect'])) {
+                $email = $_SESSION['email'];
+                $result = DbUtilisateur::getInfoUser($email);
+                $infoVehicule = DbUtilisateur::getVehicule($email);
+                include 'vue/vueUtilisateur/v_profilUtilisateur.php';
+            } else {
+                header('location: index.php');
+            }
+    
+            break;
+
+    // si l'action est ajoutVehicule, on ajoute le véhicule de l'utilisateur via la fonction ajoutVehicule() de la class dbUtilisateur
+    case 'ajoutVehicule':
+    
+        $marque = $_POST['marque'];
+       $carburant =  $_POST['carburant'];
+        
+       
+    
+        $result = DbUtilisateur::ajoutVehicule($marque, $carburant, $_SESSION['email']);
+        header('location: index.php?ctl=utilisateur&action=profil');
+>>>>>>> Stashed changes
         break;
           
     }
--- model/connectPdo.php
+++ model/connectPdo.php
@@ -10,7 +10,7 @@ class connectPdo
 		 
 		 if(!isset(self::$db))
 		 { 
-		  self::$db = new PDO('mysql:host=localhost;dbname=note_de_frais', 'root', '');  // PDO Connexion base de données 
+		  self::$db = new PDO('mysql:host=localhost;dbname=note_de_frais', 'root', 'root');  // PDO Connexion base de données 
 		  self::$db ->query('SET NAMES utf8'); 
 		  self::$db->query('SET CHARACTER SET utf8');   
 		 } 

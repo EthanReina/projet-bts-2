a81d77877aa1c9523707d43193741cc5187142cf
majidbenmerah <99648395+majidbenmerah@users.noreply.github.com>
2022-10-21 09:48:16 +0200

    Update formConnect.php

--- vue/vueUtilisateur/formConnect.php
+++ vue/vueUtilisateur/formConnect.php
@@ -6,12 +6,12 @@
       <div class="container w-25 pt-5">
         <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Adresse E-Mail</label>
-        <input type="email" class="form-control" name="email" aria-describedby="emailHelp">
+        <input type="email" class="form-control" name="email" aria-describedby="emailHelp" required>
       </div>
 
       <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Mot de Passe</label>
-        <input type="password" class="form-control" name="password">
+        <input type="password" class="form-control" name="password" required>
       </div>
 
       <button type="submit" class="btn btn-primary">Connexion</button>

97fc06bf089c587918afb16a35773805db2844b7
Ethan Reina <99647302+MisterTyra@users.noreply.github.com>
2023-11-04 21:08:33 +0100

    Update pied.php

--- vue/pied.php
+++ vue/pied.php
@@ -6,6 +6,9 @@
       <div class="row">
         <div class="col-12">
           <p class="text-center pt-3">© 2022 - Projet Note de Frais - Ethan Reina / Majid Benmerah / Kawtar Bouaaza</p>
+          <p class="text-center pt-3">Compte utilisateur : identifiant : ethan@reina mot de passe : ethan</p>
+          <p class="text-center pt-3">Compte admin : identifiant admin@admin mot de passe : admin</p>
+          
         </div>
       </div>
     </div>
